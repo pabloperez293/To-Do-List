47035726dec9e058e474b7b7ab3923cf838796d8
Pablo Perez <paperezpablo@gmail.com>
2024-04-29 01:14:23 -0300

    Agregando lista de actualizacion

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { AiFillEdit } from "react-icons/ai";
 import { BsFillTrashFill } from "react-icons/bs";
 
-const Todo = ({ id, task, isEditing }) => {
+const Todo = ({ id, task, isEditing, deleteTodo, editTodo }) => {
   return (
     <div className='flex justify-between items-center bg-violet-800 py-3 px-4 rounded-md mb-1 cursor-pointer'>
       <p className='font-primary'>{task}</p>
       <div className='flex items-center gap-x-4'>
-        <AiFillEdit className='text-xl' />
-        <BsFillTrashFill className='text-xl' />
+        <AiFillEdit className='text-xl' onClick={() => editTodo(id)} />
+        <BsFillTrashFill
+          className='text-xl'
+          onClick={() => deleteTodo(id)}
+        />
       </div>
     </div>
   );
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Form from "../Form/Form";
 import { v4 as uuidv4 } from "uuid";
 import Todo from "../Todo/Todo";
+import Edit from "../Edit/Edit"; 
 
 
 const TodoList = () => {
@@ -14,11 +15,28 @@ const TodoList = () => {
     ]);
   };
 
+  const deleteTodo = id => {
+    setTodoValue(todoValue.filter(todo => todo.id !== id))
+  }
+
+  const editTodo = id => {
+    setTodoValue(todoValue.map(todo => todo.id === id ? {...todo, isEditing:!todo.isEditing}: todo))
+  }
+
+  const editTask = (task, id) => {
+    setTodoValue(todoValue.map(todo => todo.id === id ? {...todo, task, isEditing: !todo.isEditing} : todo))
+  }
+
+  
   return (
     <div className='container bg-blue-500 mt-20 p-8 rounded-md'>
       <Form createTodo={createTodo} />
       {todoValue.map(todo => (
-        <Todo {...todo} key={todo.id} />
+        todo.isEditing ? (
+          <Edit key={todo.id} editTodo={editTask} task={todo} createTodo={createTodo} />
+        ): (
+        <Todo {...todo} key={todo.id} deleteTodo={deleteTodo} editTodo={editTodo} />
+      )
       ))}
     </div>
   );
