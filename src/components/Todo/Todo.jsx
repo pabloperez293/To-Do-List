0e0075e44cd1d8781b1c556b0ce7b0410bb3b973
Pablo Perez <paperezpablo@gmail.com>
2024-04-28 22:13:11 -0300

    actualice uuid

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import { AiFillEdit } from "react-icons/ai";
 import { BsFillTrashFill } from "react-icons/bs";
 
-const Todo = () => {
+const Todo = ({ task }) => {
   return (
     <div className='flex justify-between items-center bg-violet-800 py-3 px-4 rounded-md mb-1 cursor-pointer'>
-      <p className="font-primary">Tarea que hacer</p>
-      <div className="flex items-center gap-x-4">
-        <AiFillEdit className="text-xl"/>
-        <BsFillTrashFill  className="text-xl"/>
-
+      <p className='font-primary'>{task.task}</p>
+      <div className='flex items-center gap-x-4'>
+        <AiFillEdit className='text-xl' />
+        <BsFillTrashFill className='text-xl' />
       </div>
     </div>
-
   );
 };
 
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Form from "../Form/Form";
 import { v4 as uuidv4 } from "uuid";
+import Todo from "../Todo/Todo";
 uuidv4();
 
 const TodoList = () => {
@@ -9,13 +10,18 @@ const TodoList = () => {
   const createTodo = todo => {
     setTodoValue([
       ...todoValue,
-      { id: uuidv4(), task: todo, isEditing: false },
+      { id: uuidv4(), task:todo, isEditing:false },
     ]);
   };
 
   return (
     <div className='container bg-blue-500 mt-20 p-8 rounded-md'>
       <Form createTodo={createTodo} />
+      {
+        todoValue.map((todo, idx) => (
+          <Todo task={todo} key={idx} />
+        ))
+      }
     </div>
   );
 };
